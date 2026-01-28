ef454340014e4693cc0769d041a4fbb9c76f9262
Cynthia <cynthiabon.perso@gmail.com>
2026-01-28 16:08:27 +0100

    Add multiple animations

--- Runtime/Scripts/ScriptableObjects/VoxelObject.cs
+++ Runtime/Scripts/ScriptableObjects/VoxelObject.cs
@@ -14,24 +14,32 @@ namespace FoxEdit
             public int[] ColorIndices;
         }
 
+        [Serializable]
+        public struct AnimationFrames
+        {
+            public int StartIndex;
+            public int FrameCount;
+        }
+
         public Bounds Bounds;
 
         public int PaletteIndex = 0;
-        public Vector3[] VoxelPositions = null;
-        public int[] VoxelIndices = null;
-        public int[] FaceIndices = null;
+        //public Vector3[] VoxelPositions = null;
+        //public int[] VoxelIndices = null;
+        //public int[] FaceIndices = null;
 
-        public int[] ColorIndices = null;
+        //public int[] ColorIndices = null;
 
-        public int FrameCount = 0;
+        //public int FrameCount = 0;
 
         public int[] InstanceStartIndices;
         public int[] InstanceCount = null;
-        public int MaxInstanceCount = 0;
+        //public int MaxInstanceCount = 0;
 
         public EditorFrameVoxels[] EditorVoxelPositions = null;
         public Mesh StaticMesh = null;
 
         public Vector4[] Vertices = null;
+        public AnimationFrames[] AnimationIndices = null;
     }
 }
